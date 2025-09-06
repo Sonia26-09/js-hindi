213c18c2f94d76d274cd976dfaf7b503f15bb358
sonia2609 <soniagarg2609@gmail.com>
2025-09-06 10:23:17 +0000

    convers

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score=true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueINNumber=Number(score)
+// console.log(typeof valueINNumber );
+// console.log(valueINNumber);
+
+//"33"=>33
+//"33abc"=>NaN not a number
+//true=>1;false=>0
+
+let isLoggedIn="Sonia"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"hitesh"=>true
+
+let someNumber =33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
