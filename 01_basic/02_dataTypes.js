666a0c045c1464fd2d2d0c208d7e0d428ffe6246
sonia2609 <soniagarg2609@gmail.com>
2025-09-06 07:32:23 +0000

    Add JS basics: variables and data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all Js code as newer version
+// alert(3+3) // we are using nodejs,not browser
+
+console.log(3+3);// code readability should be high
+console.log("Sonia")
+
+let name="Sonia"
+let age=19
+let isLoggedIn=false
+let state=null
+
+//number =>2 to power 53 range
+//bigint big numerber it is usually use in stockmarket ,trading,facebbok
+//string=> int can be use in '' or "" prefer ""
+//boolean=>true/false
+// null => standalone value
+// undefined=> jb aapne variable declare kiya but value define nhi ki hai null means empty
+//symbol =>unique
+
+
+//object
+console.log(typeof "Sonia");
+console.log(typeof age);
+console.log(typeof null);//null is object interview question
+console.log(typeof undefined); //undefined aapne aap m ek type hai
